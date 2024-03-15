ec46c3c1aa9797358536ef3d7946d62107100267
kkkyaho@naver.com <kkkyaho@naver.com>
2024-03-15 19:39:50 +0900

    test

---
+++ luceneTest/src/main/java/com/mystudy/lucenetest/LuceneTestApplication.java
@@ -0,0 +1,13 @@
+package com.mystudy.lucenetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuceneTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuceneTestApplication.class, args);
+    }
+
+}
---
+++ luceneTest/src/test/java/com/mystudy/lucenetest/LuceneTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystudy.lucenetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuceneTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
